650449f05d9a2fd5710361278780841cdc97761c
GuoLizhi <lizhi.guo@foxmail.com>
2019-10-22 16:20:01 +0800

    add: leetcode 013

---
+++ data-structure-practice/0013_roman_to_integer.ts
@@ -0,0 +1,40 @@
+/**
+ * 将罗马数字转化成整数
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ */
+var romanToInt = function(s: string) {
+  let map = {
+    'I': 1,
+    'IV': 4,
+    'V': 5,
+    'IX': 9,
+    'X': 10,
+    'XL': 40,
+    'L': 50,
+    'XC': 90,
+    'C': 100,
+    'CD': 500,
+    'CM': 900,
+    'M': 1000
+  };
+
+  let ans: number = 0;
+  for (let i = 0; i < s.length;) {
+    if (i + 1 < s.length && map[s.substring(i, i + 2)]) {
+      ans += map[s.substring(i, i + 2)];
+      i += 2;
+    } else {
+      ans += map[s.substring(i, i + 1)];
+      i += 1
+    }
+  }
+
+  return ans;
+};
