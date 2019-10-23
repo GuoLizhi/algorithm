dca9801ceae0acd55a80ce922e5c6b48b92baf63
GuoLizhi <lizhi.guo@foxmail.com>
2019-10-23 14:29:00 +0800

    add: leetcode 013 and 028

--- data-structure-practice/0013_roman_to_integer.ts
+++ data-structure-practice/0013_roman_to_integer.ts
@@ -11,18 +11,19 @@
  */
 var romanToInt = function(s: string) {
   let map = {
-    'I': 1,
-    'IV': 4,
-    'V': 5,
-    'IX': 9,
-    'X': 10,
-    'XL': 40,
-    'L': 50,
-    'XC': 90,
-    'C': 100,
-    'CD': 500,
-    'CM': 900,
-    'M': 1000
+    I : 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
   };
 
   let ans: number = 0;
