21ae5fafa437f2e7781ae500c024b722169b614c
GuoLizhi <lizhi.guo@foxmail.com>
2019-10-25 11:08:30 +0800

    update directory structure

--- algorithm/HashTable/0001_two_sum.ts
+++ algorithm/HashTable/0001_two_sum.ts
@@ -1,10 +1,10 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @author lizhi.guo@foxmail.com
+ * @source https://leetcode-cn.com/problems/two-sum/
+ * @time   2019-10-25
  */
 // 第一种方式，直接采用两重循环，时间复杂度为O(n^2)，空间复杂度为O(1)
-var twoSum = function(nums, target) {
+const twoSum = function(nums: number[], target: number) {
   const len = nums.length;
   for (let i = 0; i < len; i++) {
     for (let j = i + 1; j < len; j++) {
@@ -16,12 +16,12 @@ var twoSum = function(nums, target) {
 };
 
 // 第二种方式，使用HashTable的方式，时间复杂度为O(n)，空间复杂度为O(n)
-var twoSum2 = function(nums, target) {
+const twoSum2 = function(nums: number[], target: number) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
     map.set(nums[i], i);
   }
-  
+
   for (let i = 0; i < nums.length; i++) {
     let index = map.get(target - nums[i]);
     if (index && index !== i) {
@@ -31,7 +31,7 @@ var twoSum2 = function(nums, target) {
 };
 
 // 第三种方式，使用HashTable，一次循环通过，时间复杂度为O(n)，空间复杂度为O(n)
-var twoSum3 = function(nums, target) {
+const twoSum3 = function(nums: number[], target: number) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
     let complete = target - nums[i];
