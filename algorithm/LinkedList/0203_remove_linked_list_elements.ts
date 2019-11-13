ca7c1b5331bb8664064ab7af07f43b510fa8f420
GuoLizhi <lizhi.guo@foxmail.com>
2019-11-14 07:57:42 +0800

    add leetcode 0203 at 20191114

---
+++ algorithm/LinkedList/0203_remove_linked_list_elements.ts
@@ -0,0 +1,47 @@
+interface ListNode {
+  val: number
+  next: ListNode
+}
+
+// 不设置虚拟头节点
+function removeElements(head: ListNode, val: number): ListNode {
+  let cur: ListNode = head;
+
+  while (head.val === val && head !== null) {
+    let delNode = head
+    head = delNode.next
+  }
+
+  if (head === null) {
+    return null
+  }
+
+  while (cur.next !== null) {
+    if (cur.next.val === val) {
+      let delNode: ListNode = cur.next
+      cur.next = delNode.next
+    } else {
+      cur = cur.next
+    }
+  }
+
+  return head
+}
+
+// 设置虚拟头节点
+function removeElements2(head: ListNode, val: number): ListNode {
+  let dummyHead: ListNode = {
+    val: null,
+    next: head
+  }
+  let cur = dummyHead
+  while (cur.next !== null) {
+    if (cur.next.val === val) {
+      let delNode: ListNode = cur.next
+      cur.next = delNode.next
+    } else {
+      cur = cur.next
+    }
+  }
+  return dummyHead.next
+}
--- algorithm/LinkedList/README.md
+++ algorithm/LinkedList/README.md
@@ -5,3 +5,4 @@
 1. [两数相加](./0002_add_two_numbers.ts)
 2. [合并两个有序链表](./0021_merge_two_sorted_lists.ts)
 3. [删除链表中的重复元素](./0083_remove_duplicates_from_sorted_list.ts)
+4. [反转链表](./0206_reverse_linked_list.ts)
