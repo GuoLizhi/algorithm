0bbc127681654b1c9ff05abf35efe633e306cabb
GuoLizhi <lizhi.guo@foxmail.com>
2019-12-19 10:33:31 +0800

    add stack Test Suites

--- data-structure/Array/Array.ts
+++ data-structure/Array/Array.ts
@@ -3,27 +3,52 @@ export default class MyArray<E> {
   public data: E[];
   public size: number = 0;
 
-  // 构造函数，传入数组的容量capacity
+  /**
+   * 构造函数，传入数组的容量capacity
+   * @param {number} capacity 数组容量，默认10
+   */
   constructor(capacity = 10) {
     this.data = new Array(capacity);
   }
 
-  // 获取数组容量
+  /**
+   * 获取数组容量（数组能总共能包含多少元素）
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {number}
+   */
   getCapacity(): number {
     return this.data.length;
   }
 
-  // 获取数组中元素的个数
+  /**
+   * 获取数组中当前存储元素的个数
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {number}
+   */
   getSize(): number {
     return this.size;
   }
 
-  // 判断数组是否为空
+  /**
+   * 判断数组是否为空，即元素的个数是否为0
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {boolean}
+   */
   isEmpty(): boolean {
     return this.size === 0;
   }
 
-  // 在数组中index位置添加一个元素
+  /**
+   * 在数组中index位置添加一个元素
+   * Time Complexity O(n)
+   * Space Complexity O(1)
+   * @param {number} index 要插入的位置
+   * @param {E} e 要插入的元素
+   * @return {void}
+   */
   add(index: number, e: E): void {
     if (index < 0 || index > this.size) {
       throw new Error('Add failed. Required index >= 0 and index <= size');
@@ -42,17 +67,36 @@ export default class MyArray<E> {
     this.size++;
   }
 
-  // 向所有元素之后添加一个元素
+  /**
+   * 向所有元素之后添加一个元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @param {E} e 要插入的元素
+   * @return {void}
+   */
   addLast(e: E): void {
     this.add(this.size, e);
   }
 
-  // 向所有元素之前添加一个元素
+  /**
+   * 向所有元素之前添加一个元素
+   * Time Complexity O(n)
+   * Space Complexity O(1)
+   * @param {E} e 要插入的元素
+   * @return {void}
+   */
   addFirst(e: E): void {
     this.add(0, e);
   }
 
-  // 修改index索引位置元素为e
+  /**
+   * 修改index索引位置元素为e
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @param {number} index 要插入元素的位置
+   * @param {E} e 要插入的元素
+   * @return {void}
+   */
   set(index: number, e: E) {
     if (index < 0 || index >= this.size) {
       throw new Error('Set failed. Index is illegal.');
@@ -60,7 +104,13 @@ export default class MyArray<E> {
     this.data[index] = e;
   }
 
-  // 查找数组中是否含有元素e
+  /**
+   * 查找数组中是否含有元素e
+   * Time Complexity O(n)
+   * Space Complexity O(1)
+   * @param {E} e 要查找的元素
+   * @return {boolean}
+   */
   contains(e: E): boolean {
     for (let i = 0; i < this.size; i++) {
       if (e === this.data[i]) {
@@ -70,7 +120,13 @@ export default class MyArray<E> {
     return false;
   }
 
-  // 查找数组中元素e所在的索引，如果不存在，则返回-1
+  /**
+   * 查找数组中元素e所在的索引，如果不存在，则返回-1
+   * Time Complexity O(n)
+   * Space Complexity O(1)
+   * @param {E} e 要查找的元素
+   * @return {boolean}
+   */
   find(e: E): number {
     for (let i = 0; i < this.size; i++) {
       if (e === this.data[i]) {
@@ -80,7 +136,13 @@ export default class MyArray<E> {
     return -1;
   }
 
-  // 从数组中删除index位置的元素，并且返回删除元素
+  /**
+   * 从数组中删除index位置的元素，并且返回删除元素
+   * Time Complexity O(n)
+   * Space Complexity O(1)
+   * @param {number} index 要删除的元素
+   * @return {E}
+   */
   remove(index: number): E {
     if (index < 0 || index > this.size) {
       throw new Error('Remove failed. index is illegal')
@@ -100,17 +162,33 @@ export default class MyArray<E> {
     return ret;
   }
 
-  // 删除数组中的最后一个元素
+  /**
+   * 删除数组中的最后一个元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {E}
+   */
   removeLast(): E {
     return this.remove(this.size - 1);
   }
 
-  // 删除数组中的第一个元素
+  /**
+   * 删除数组中的第一个元素
+   * Time Complexity O(n)
+   * Space Complexity O(1)
+   * @return {E}
+   */
   removeFirst(): E {
     return this.remove(0);
   }
 
-  // 获取index索引的元素
+  /**
+   * 获取index索引的元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @param {number} index 要获取元素的索引
+   * @return {E}
+   */
   get(index: number): E {
     if (index < 0 || index >= this.size) {
       throw new Error('Get failed. Index is illegal...')
@@ -118,17 +196,33 @@ export default class MyArray<E> {
     return this.data[index];
   }
 
-  // 获取数组中的第一个元素
+  /**
+   * 获取数组中的第一个元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {E}
+   */
   getFirst() {
     return this.get(0);
   }
 
-  // 获取数组中的最后一个元素
+  /**
+   * 获取数组中的最后一个元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {E}
+   */
   getLast() {
     return this.get(this.size - 1);
   }
 
-  // 数组扩容，或者缩容操作
+  /**
+   * 数组扩容，或者缩容操作
+   * Time Complexity O(n)
+   * Space Complexity O(n)
+   * @param {number} newCapacity 新的数组的容量
+   * @return {void}
+   */
   resize(newCapacity: number): void {
     let newData: E[] = new Array(newCapacity);
     for (let i = 0; i < this.size; i++) {
--- data-structure/Stack/ArrayStack.ts
+++ data-structure/Stack/ArrayStack.ts
@@ -9,56 +9,74 @@ interface Stack<E> {
 }
 
 export default class ArrayStack<E> implements Stack<E> {
-  private array: MyArray<E>;
+  public array: MyArray<E>;
 
+  /**
+   * 构造函数，传入栈的容量capacity
+   * @param {number} capacity 数组容量，默认10
+   */
   constructor(capacity: number = 10) {
     this.array = new MyArray<E>(capacity);
   }
 
+  /**
+   * 获取栈容量（栈能总共能包含多少元素）
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {number}
+   */
   getCapacity(): number {
     return this.array.getCapacity();
   }
 
+  /**
+   * 获取栈中当前存储元素的个数
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {number}
+   */
   getSize(): number {
     return this.array.getSize();
   }
 
+  /**
+   * 判断栈是否为空，即元素的个数是否为0
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {boolean}
+   */
   isEmpty(): boolean {
     return this.array.isEmpty();
   }
 
+  /**
+   * 在栈顶添加一个元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @param {E} e 要插入的元素
+   * @return {void}
+   */
   push(e: E): void {
     this.array.addLast(e);
   }
 
+  /**
+   * 在栈顶出栈一个元素，并返回出栈的元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {E}
+   */
   pop(): E {
     return this.array.removeLast();
   }
 
+  /**
+   * 查看栈顶元素
+   * Time Complexity O(1)
+   * Space Complexity O(1)
+   * @return {E}
+   */
   peek(): E {
     return this.array.getLast();
   }
-
-  toString(): string {
-    let res = '';
-    res += 'Stack: ';
-    res += '[';
-    for (let i = 0; i < this.getSize(); i++) {
-      res += this.array.get(i);
-      if (i !== this.array.getSize() - 1) {
-        res += ', ';
-      }
-    }
-    res += '] top';
-
-    return res;
-  }
 }
-
-// let s = new ArrayStack<number>();
-// for (let i = 0; i < 5; i++) {
-//   s.push(i);
-//   console.log(s.toString());
-// }
-// s.pop();
-// console.log(s.toString());
