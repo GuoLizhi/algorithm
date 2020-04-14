7758e39ef4606a38fe82897fb01e2afcbc13e589
GuoLizhi <lizhi.guo@foxmail.com>
2020-04-14 22:20:15 +0800

    UPDATE LinkedList

--- DataStructureJava/LinkedListStack.java
+++ DataStructureJava/LinkedListStack.java
@@ -1,4 +1,8 @@
-
+/**
+ * 使用链表实现的栈，由于栈的操作都是在线性数据结构的一端完成；
+ * 而由于链表的头部添加和删除元素都是O(1)的时间复杂度，因此我们可以用链表的头部连充当栈顶元素
+ * 这样实现可以达到入栈和出栈操作都是O(1)时间复杂度
+ */
 public class LinkedListStack<E> implements Stack<E> {
     private LinkedList<E> list;
 
